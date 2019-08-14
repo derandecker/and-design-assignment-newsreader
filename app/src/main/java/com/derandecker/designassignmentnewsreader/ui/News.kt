2caf244e66f2d28494178b184060b50175802962
Deran Decker <me@derandecker.com>
2019-08-14 10:05:11 -1000

    Add color to login page. Add News (List Page) Activity.

---
+++ app/src/main/java/com/derandecker/designassignmentnewsreader/ui/News.kt
@@ -0,0 +1,15 @@
+package com.derandecker.designassignmentnewsreader.ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.derandecker.designassignmentnewsreader.R
+
+class News : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news)
+
+
+    }
+}
